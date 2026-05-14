79e688176dc9ea72dce15c9b670e75bfb55c1643
Islom <iamsheraliyev@gmail.com>
2026-05-14 15:08:03 +0500

    Remove legacy XML colors

---
+++ design/src/main/kotlin/uz/yalla/sdk/android/design/YallaThemedImage.kt
@@ -0,0 +1,18 @@
+package uz.yalla.design.color
+
+import uz.yalla.sdk.android.design.R
+
+enum class YallaThemedImage(val resId: Int) {
+    BlurryLogo(R.drawable.img_blurry_logo),
+    CloseCircle(R.drawable.img_close_circle),
+    Login(R.drawable.img_login),
+    Logout(R.drawable.img_logout),
+    MapPin(R.drawable.img_map_pin),
+    NotificationMute(R.drawable.img_notification_mute),
+    OrderHistory(R.drawable.img_order_history),
+    OrderSearch(R.drawable.img_order_search),
+    Safety(R.drawable.img_safety),
+    ShieldCheck(R.drawable.img_shield_check),
+    TariffCard(R.drawable.img_tariff_card),
+    TrashCan(R.drawable.img_trash_can);
+}
